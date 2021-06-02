80bb551ce81d64096ecb6cee1c24168826e1a8e5
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2021-06-02 16:01:47 +0500

    change name of admin page

--- base/admin.py
+++ base/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 
 from base.models import Task
 
+admin.site.site_header = 'Task Admin Dashboard'
+
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    search_fields = ('title', 'user')
-    list_display = ('title', 'user', 'description', 'complete', 'created_at')
+    search_fields = ('title',)
+    list_display = ('title', 'user', 'description', 'created_at', 'complete')
     list_filter = ('complete',)
