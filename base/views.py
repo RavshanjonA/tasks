133c88a7333b685abf7dee661b16b1fb9a1c9dd5
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2021-06-02 16:03:19 +0500

    crud action for task model

--- base/views.py
+++ base/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from base.models import Task
+
+
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    context_object_name = 'task'
+    fields = '__all__'
+    template_name = 'base/task_create.html'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    fields = '__all__'
+    model = Task
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+
