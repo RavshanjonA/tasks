e25218f92dee54e366866e39001fee3862b2e483
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2021-06-02 12:05:55 +0500

    register model

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from base.models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'user')
+    list_display = ('title', 'user', 'description', 'complete', 'created_at')
+    list_filter = ('complete',)
