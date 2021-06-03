9f358d67b86ff244f60608641545a17fef03365f
Ahmadjonov-98 <akhmadjonovr.98@gmail.com>
2021-06-03 12:43:57 +0500

    change titles in admin page

--- base/admin.py
+++ base/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from base.models import Task
 
 admin.site.site_header = 'Task Admin Dashboard'
-
+admin.site.index_title = 'Task Member Adminstrations'
+admin.site.site_title = 'Log in'
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
